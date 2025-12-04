4722bee8fe1c5ab42f1c21ad570cf8c2b4511ce3
JavierCaicedoTobon <javiercaicedotobon@gmail.com>
2025-12-04 11:52:15 -0500

    deje el proyecto con la configuracion inicial

--- liberal/settings/base.py
+++ liberal/settings/base.py
@@ -175,6 +175,18 @@ LOCAL_APPS = (
     'applications.equipo',
 )
 
+# Configuración CKEditor
+CKEDITOR_UPLOAD_PATH = "uploads/"
+CKEDITOR_IMAGE_BACKEND = "pillow"
+CKEDITOR_JQUERY_URL = '//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js'
+CKEDITOR_CONFIGS = {
+    'default': {
+        'toolbar': 'full',
+        'height': 300,
+        'width': '100%',
+    },
+}
+
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
